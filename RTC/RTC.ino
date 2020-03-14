6b4f657046e6c71d41951609cbf162a5a6000461
billbsing <bill.barman@dex.sg>
2020-03-14 20:42:58 +0800

    save time running

--- RTC/RTC.ino
+++ RTC/RTC.ino
@@ -3,6 +3,7 @@
 #include <WifiUDP.h>
 #include <NTPClient.h>
 #include <TimeLib.h>
+#include <FS.h>
 #include "private.h"
 
 #define DEBUG_SERIAL
@@ -13,12 +14,89 @@
 #define CLOCK_RADIUS                36
 #define PI                          3.1415926535897932384626433832795
 
+
+#define TIME_DATA_FILE              "/timeData.dat"
+
+
 WiFiUDP ntpUDP;
 NTPClient ntpClient(ntpUDP, "sg.pool.ntp.org", 8 * SECS_PER_HOUR);
 bool isSyncTime;
 RTC_TimeTypeDef rtcTimeStruct;
 RTC_DateTypeDef rtcDateStruct;
 uint8_t lastDisplayMinute;
+
+typedef struct {
+    time_t startTime;
+    time_t lastTime;
+} TimeData;
+
+TimeData timeData;
+
+
+void saveTimeData() {
+
+    SPIFFS.begin();
+    File f = SPIFFS.open(TIME_DATA_FILE, "w");
+    if ( f ) {
+        f.write((uint8_t *)&timeData, sizeof(TimeData));
+/*
+        uint8_t *ptr = (uint8_t *) &timeData;
+        for (uint16_t i = 0 ; i <sizeof(TimeData); i++ ) {
+            f.write(*ptr);
+            ptr ++;
+        }
+*/
+        Serial.println(timeData.startTime);
+        Serial.println(timeData.lastTime);
+
+        f.close();
+    }
+    SPIFFS.end();
+}
+
+void setTimeData() {
+    M5.Rtc.GetTime(&rtcTimeStruct);
+    M5.Rtc.GetData(&rtcDateStruct);
+    setTime(rtcTimeStruct.Hours, rtcTimeStruct.Minutes, rtcTimeStruct.Seconds, \
+    rtcDateStruct.Date, rtcDateStruct.Month, rtcDateStruct.Year);
+
+    if ( timeData.startTime == 0) {
+        timeData.startTime = now();
+    }
+    timeData.lastTime = now();
+}
+void readTimeData() {
+    SPIFFS.begin();
+    memset(&timeData, 0, sizeof(TimeData));
+    if (SPIFFS.exists(TIME_DATA_FILE)) {
+        File f = SPIFFS.open(TIME_DATA_FILE, "r");
+        if ( f ) {
+            f.readBytes((char *) &timeData, sizeof(TimeData));
+/*
+            uint8_t *ptr = (uint8_t *) &timeData;
+            for (uint16_t i = 0 ; i <sizeof(TimeData); i++ ) {
+                *ptr = f.read();
+                ptr ++;
+            }
+*/
+            f.close();
+        }
+        Serial.println(timeData.startTime);
+        Serial.println(timeData.lastTime);
+    }
+    SPIFFS.end();
+}
+
+void clearTimeData() {
+    Serial.println("clear time");
+/*
+    SPIFFS.begin();
+    SPIFFS.format();
+    SPIFFS.end();
+*/
+    timeData.startTime = now();
+    saveTimeData();
+}
 
 float calcMinuteToX(int minute, int radius) {
     float angle = (((float) minute / 60) * 360) - 90;
@@ -39,25 +117,27 @@ void showClockHand(uint16_t position, uint16_t radius, uint16_t baseWidth, uint1
     uint16_t hourToX = clockLeft + calcMinuteToX(position, radius);
     uint16_t hourToY = clockTop + calcMinuteToY(position, radius);
     M5.Lcd.fillTriangle(hourFromTopX, hourFromTopY, hourToX, hourToY, hourFromBottomX, hourFromBottomY, color);
-
+    M5.Lcd.drawLine(clockLeft, clockTop, hourToX, hourToY, color);
 }
 void showClock() {
     int clockLeft = SCREEN_WIDTH - CLOCK_RADIUS - 10;
     int clockTop = CLOCK_RADIUS + 2;
-    M5.Lcd.fillRect(clockLeft - CLOCK_RADIUS, clockTop - CLOCK_RADIUS,CLOCK_RADIUS * 2, CLOCK_RADIUS*2, BLACK);
+    // M5.Lcd.fillRect(clockLeft - CLOCK_RADIUS, clockTop - CLOCK_RADIUS,CLOCK_RADIUS * 2, CLOCK_RADIUS*2, BLACK);
     M5.Lcd.drawCircle(clockLeft, clockTop, CLOCK_RADIUS, RED);
     for ( int minute = 0; minute < 60 ; minute += 5) {
-        int fromX = clockLeft + calcMinuteToX(minute, CLOCK_RADIUS - 4);
-        int fromY = clockTop + calcMinuteToY(minute, CLOCK_RADIUS - 4);
+        int fromX = clockLeft + calcMinuteToX(minute, CLOCK_RADIUS - 1);
         int toX = clockLeft + calcMinuteToX(minute, CLOCK_RADIUS);
+        int fromY = clockTop + calcMinuteToY(minute, CLOCK_RADIUS - 1);
         int toY = clockTop + calcMinuteToY(minute, CLOCK_RADIUS);
 
-        M5.Lcd.drawLine(fromX, fromY, toX, toY, WHITE);
+        // M5.Lcd.drawLine(fromX, fromY, toX, toY, WHITE);
+        M5.Lcd.fillCircle(toX, toY, 1, WHITE);
     }
-
+    // int totalHourMinutes = (rtcTimeStruct.Hours * 60) + rtcTimeStruct.Minutes;
+    //int hourInMinutes = map(totalHourMinutes, 0, 1440, 0 , 60);
     int hourInMinutes = (rtcTimeStruct.Hours % 12) * 5;
     showClockHand(hourInMinutes, CLOCK_RADIUS / 2, 6, 4, GREEN);
-    showClockHand(rtcTimeStruct.Minutes, CLOCK_RADIUS - 5, 2, 4, YELLOW);
+    showClockHand(rtcTimeStruct.Minutes, CLOCK_RADIUS - 5, 4, 4, YELLOW);
     // M5.Lcd.fillCircle(clockLeft, clockTop, 4, BLACK);
 }
 
@@ -65,7 +145,8 @@ void showTime() {
     M5.Rtc.GetTime(&rtcTimeStruct);
     M5.Rtc.GetData(&rtcDateStruct);
     if ( lastDisplayMinute != rtcTimeStruct.Minutes) {
-        M5.Lcd.setCursor(0, 2);
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(0, SCREEN_HEIGHT - 20);
         M5.Lcd.setTextSize(2);
         // M5.Lcd.printf("Data: %04d-%02d-%02d\n",rtcDateStruct.Year, rtcDateStruct.Month,rtcDateStruct.Date);
         // M5.Lcd.printf("Week: %d\n",RTC_DateStruct.WeekDay);
@@ -79,8 +160,21 @@ void showTime() {
             M5.Lcd.print("am");
         }
         */
+
         showClock();
+        setTimeData();
+
+        int seconds = timeData.lastTime - timeData.startTime;
+        int hours = floor(seconds / 3600);
+            seconds -= hours * 3600;
+        int minutes = floor(seconds / 60);;
+            seconds -= minutes * 60;
+
+        M5.Lcd.setTextSize(1);
+        M5.Lcd.setCursor(5, 2);
+        M5.Lcd.printf("%d:%02d:%02d", hours, minutes, seconds);
         lastDisplayMinute = rtcTimeStruct.Minutes;
+        saveTimeData();
     }
 
 
@@ -111,9 +205,8 @@ bool syncTime() {
         result = true;
     }
 }
+
 void setup() {
-
-
     #ifdef DEBUG_SERIAL
     Serial.begin(115200);
     Serial.setTimeout(2000);
@@ -128,7 +221,7 @@ void setup() {
     digitalWrite(LED_PIN, 1);
 
     M5.begin();
-    M5.Lcd.setRotation(3);
+    M5.Lcd.setRotation(1);
     M5.Lcd.fillScreen(BLACK);
 
 //    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -136,6 +229,8 @@ void setup() {
     isSyncTime = false;
     lastDisplayMinute = 0xFF;
     // ntpClient.setOnSync(onSync);
+    readTimeData();
+
 }
 
 void loop() {
@@ -147,6 +242,8 @@ void loop() {
     M5.BtnA.read();
     if ( M5.BtnA.isPressed() ) {
         isSyncTime = false;
+        clearTimeData();
+        lastDisplayMinute = 0xFF;
     }
     // delay(500);
     M5.Axp.LightSleep(1000 * 1000);
