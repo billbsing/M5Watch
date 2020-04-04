76e656692c368f3582535e007b3027d9aa18dd89
billbsing <bill.barman@dex.sg>
2020-04-04 13:52:31 +0800

    show external libraries

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Based on ESP32
 M5StickC ESP32-PICO Mini IoT Development Kit
 
 see [M5Watch](https://m5stack.com/collections/m5-core/products/stick-c?variant=17203451265114)
+
+
+### Libraries
+
+This project use the following libraries:
+
+[M5StickC](https://github.com/m5stack/M5StickC.git)
+[NTPClient](https://github.com/arduino-libraries/NTPClient.git)
+[TimeLib](https://github.com/PaulStoffregen/Time.git)
--- getBuildEnvironment.sh
+++ getBuildEnvironment.sh
@@ -4,14 +4,16 @@ rm -rf buildEnvironment
 mkdir buildEnvironment
 cd buildEnvironment
 
+echo "get makeEspArduino"
 git clone https://github.com/plerup/makeEspArduino.git
+echo "get arduno-esp32"
 git clone https://github.com/espressif/arduino-esp32.git esp32Arduino
-echo "get esp32Arduino"
 cd esp32Arduino/tools
+echo "build arduno-esp32"
 python get.py
-cd ../../
-git clone --recursive https://github.com/espressif/esp-idf.git
-cd esp-idf/tools
-instal..sh
 
 
+# cd ../../
+# git clone --recursive https://github.com/espressif/esp-idf.git
+# cd esp-idf/tools
+# instal..sh
